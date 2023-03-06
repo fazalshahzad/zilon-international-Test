49b9e381007faafc66bd5155db26230a273f0351
fazalshahzad <shancompany322@gmail.com>
2023-03-06 19:17:45 +0500

    c

---
+++ Zilon-test/src/app/data-filter/data-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-filter',
+  templateUrl: './data-filter.component.html',
+  styleUrls: ['./data-filter.component.css']
+})
+export class DataFilterComponent {
+
+}
